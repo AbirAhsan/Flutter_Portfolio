949ea299ea202850df14292f4af518e6c01e0f06
Abir Ahsan <abirahsan122@gmail.com>
2021-02-20 00:51:23 +0600

    add favicon and title

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Abir Ahsan',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
